e684a45be4d08081d5fcea64d0bed86fae7a319a
williansjaime <williansjaime@outlook.com>
2023-06-15 22:22:07 -0300

    Adicioinando modelo a api

--- Aplicacao.py
+++ Aplicacao.py
@@ -1,14 +1,31 @@
 # Contruir a API --> Flask
+
+import joblib
 from flask import Flask, request
 
 
 #Instanciar o aplicativo
 aplicativo = Flask(__name__)
 
-#Funcao para receber uma informacao
-@aplicativo.route('/API_Preditivo/<Parametro1>', methods=['GET'])
-def Funcao_01(Parametro1):
-    return {'Retorno':f'{Parametro1}'}
+#Carregar Modelo
+Modelo = joblib.load("Modelo_Floresta_Aleatorio_v100.pkl")
 
+#Funcao para receber uma informacao
+@aplicativo.route('/API_Preditivo/<area>;<rooms>;<bathroom>;<parking_spaces>;<floor>;<animal>;<furniture>;<hoa>;<property_tax>;', methods=['GET'])
+def Funcao_01(area, rooms, bathroom, parking_spaces, floor, animal, furniture, hoa, property_tax):
+    #Recebendo os inputs da Api
+    Lista = [
+        float(area), float(rooms), float(bathroom), float(parking_spaces), 
+        float(floor), float(animal), float(furniture), float(hoa), float(property_tax)
+    ]
+
+    #Tentar a Previsao
+    try:
+        #Predict
+        Previsao = Modelo.predict(Lista)
+        return {'Valor_Aluguel':Previsao}
+    except:
+        return {'Aviso':'Deu algum erro!'}
+    
 if __name__ == '__main__':
     aplicativo.run(debug=True)
